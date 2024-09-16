4d1de236943dd38ecad1b755678633a4c095f660
camilanv <102964236+camilanv@users.noreply.github.com>
2024-09-16 09:52:05 -0300

    adicionei codigos

--- Graficos/informaçoes globais.js
+++ Graficos/informaçoes globais.js
@@ -1,6 +1,16 @@
 
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
+{
+
+  "total_pessoas_conectadas": 5.04e9,
+
+"tempo_medio": 2.38,
+
+"total_pessoas_mundo": 7.888e9
+
+}
+
 <script type="module" src="graficos/informacoesGlobais.js"></script>
 
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
@@ -28,27 +38,7 @@ async function visualizarInformacoesGlobais() {
         }
     
         visualizarInformacoesGlobais()
-        async function vizualizarInformacoesGlobais() {
-            const res = await fetch(url)
-            const dados = await res.json()
-            cosole.log(dados);
-            const paragrafo = document.createElement('p')
-            paragrafo.classList.add('graficos-container__texto')
-          }
-          
-          vizualizarInformacoesGlobais()
-          paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
-          async function vizualizarInformacoesGlobais() {
-            const res = await fetch(url)
-            const dados = await res.json()
-            cosole.log(dados);
-            const paragrafo = document.createElement('p')
-            paragrafo.classList.add('graficos-container__texto')
-            paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
-          }
-          
-          vizualizarInformacoesGlobais()
-          async function vizualizarInformacoesGlobais() {
+…          async function vizualizarInformacoesGlobais() {
             const res = await fetch(url)
             const dados = await res.json()
             cosole.log(dados);
