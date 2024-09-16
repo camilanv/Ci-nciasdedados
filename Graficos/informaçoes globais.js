c7757a90899510185b59f62e604e853e58b1ee65
camilanv <102964236+camilanv@users.noreply.github.com>
2024-09-16 09:49:47 -0300

    .

--- Graficos/informaçoes globais.js
+++ Graficos/informaçoes globais.js
@@ -28,4 +28,40 @@ async function visualizarInformacoesGlobais() {
         }
     
         visualizarInformacoesGlobais()
-    
+        async function vizualizarInformacoesGlobais() {
+            const res = await fetch(url)
+            const dados = await res.json()
+            cosole.log(dados);
+            const paragrafo = document.createElement('p')
+            paragrafo.classList.add('graficos-container__texto')
+          }
+          
+          vizualizarInformacoesGlobais()
+          paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+          async function vizualizarInformacoesGlobais() {
+            const res = await fetch(url)
+            const dados = await res.json()
+            cosole.log(dados);
+            const paragrafo = document.createElement('p')
+            paragrafo.classList.add('graficos-container__texto')
+            paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+          }
+          
+          vizualizarInformacoesGlobais()
+          async function vizualizarInformacoesGlobais() {
+            const res = await fetch(url)
+            const dados = await res.json()
+            cosole.log(dados);
+            const paragrafo = document.createElement('p')
+            paragrafo.classList.add('graficos-container__texto')
+            paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+            
+            const container = document.getElementById(‘graficos-container’)
+            container.appendChild(paragrafo)
+          }
+          
+          vizualizarInformacoesGlobais()
+          .graficos-container {
+            margin: 5rem;
+           }
+           
